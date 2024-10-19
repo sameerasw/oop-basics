017d0acb461bdd723163f1aa55414ff1483b5d4e
sameerasw <sameera.whoami@gmail.com>
2024-10-19 10:23:56 +0530

    Week 04

--- eCommerceDelivery/src/Main.java
+++ eCommerceDelivery/src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 
+        // when using polymorphism, we can use the parent class to create objects. So first we write the parent class, then the object name, and then the child class.
         Delivery airDelivery = new AirDelivery("123 Main St", 100);
         airDelivery.calculateCost( 100 );
         airDelivery.trackProgress();
